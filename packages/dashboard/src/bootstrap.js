e9a0712e7e5d619e826cc6a9cda3f5a5ef3194ff
Michael Caballero <mcaballero.arts@gmail.com>
2026-03-13 11:20:09 -0400

    Initial Dashboard app

---
+++ packages/dashboard/src/bootstrap.js
@@ -0,0 +1,23 @@
+import {createApp } from 'vue';
+import Dashboard from './components/Dashboard';
+
+const mount = (el) => {
+    console.log('mount');
+    
+    const app = createApp(Dashboard);
+    app.mount(el);
+};
+
+console.log('process.env.NODE_ENV', process.env.NODE_ENV);
+// running standalone
+if (process.env.NODE_ENV === 'development') {
+    const devRoot = document.querySelector('#_dashboard-dev-root');
+    
+    if (devRoot) {
+        mount(devRoot);
+    }
+}
+
+
+// running through container
+export { mount };
---
+++ packages/dashboard/src/index.js
@@ -0,0 +1,2 @@
+console.log('bs');
+import('./bootstrap');
